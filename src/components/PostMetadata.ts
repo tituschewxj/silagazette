ba779ed0f9ac27334201a9295fff55f847fd95da
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2023-12-19 21:29:10 +0800

    refactor: PostMetadata type

--- src/components/PostMetadata.ts
+++ src/components/PostMetadata.ts
@@ -1,4 +1,4 @@
-export interface PostMetadata {
+export type PostMetadata = {
     title: string;
     subtitle: string;
     author: string;
@@ -6,4 +6,4 @@ export interface PostMetadata {
     tags: string[];
     hidden: boolean;
     slug: string;
-}
+};
