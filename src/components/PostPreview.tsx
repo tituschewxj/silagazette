07d7b27561b3e71eda8990708a3c98ca93c7799c
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2023-12-21 16:17:48 +0800

    feat: add tags to PostPreview

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -9,25 +9,25 @@ const PostPreview = (props: { post: PostData }) => {
     const post = props.post;
     return (
         <>
-            <Link href={`/blog/${slug}`}>
-                <div className='group my-8 rounded-lg p-8 shadow-md transition duration-200 hover:bg-gray-100'>
-                    <h1 className='font-serif text-2xl font-bold tracking-tight transition duration-150 group-hover:text-red-700 group-hover:underline'>
-                        {post.data.title}
-                    </h1>
+            <div className='group relative my-8 rounded-lg p-8 shadow-md transition duration-200 hover:bg-gray-100'>
+                <h1 className='font-serif text-2xl font-bold tracking-tight transition duration-150 group-hover:text-red-700 group-hover:underline'>
+                    {post.data.title}
+                </h1>
 
-                    <h2 className='text-md text-slate-700'>
-                        {post.data.subtitle}
-                    </h2>
-                    <div className='text-sm text-slate-500'>
-                        Published on {getFormattedDate(post.data.date)}
-                    </div>
-                    <div className='text-sm text-slate-500'>
-                        By {post.data.author}
-                    </div>
-                    {/* NOTE: cannot nest anchor */}
-                    {/* <TagsList tags={post.data.tags} /> */}
+                <h2 className='text-md text-slate-700'>{post.data.subtitle}</h2>
+                <div className='text-sm text-slate-500'>
+                    Published on {getFormattedDate(post.data.date)}
                 </div>
-            </Link>
+                <div className='text-sm text-slate-500'>
+                    By {post.data.author}
+                </div>
+                {/* NOTE: cannot nest anchor tags */}
+                <TagsList tags={post.data.tags} />
+                <Link
+                    className='absolute inset-0 h-full w-full'
+                    href={`/blog/${slug}`}
+                ></Link>
+            </div>
         </>
     );
 };
