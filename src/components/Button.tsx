1c6102821e603eb46f0f553ee8eff9314487a0bd
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2023-12-22 14:28:33 +0800

    feat: improve button hover

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ const Button = (props: { href: string; children?: React.ReactNode }) => {
         <div className='mx-auto py-8 text-center'>
             <Link
                 href={props.href}
-                className='rounded-full bg-white px-4 py-2 font-semibold text-red-700 shadow-md transition duration-200 hover:bg-gray-100'
+                className='rounded-full bg-white px-4 py-2 font-semibold text-red-700  shadow-md transition duration-200 hover:bg-gray-100 hover:text-red-600'
             >
                 {props.children}
             </Link>
