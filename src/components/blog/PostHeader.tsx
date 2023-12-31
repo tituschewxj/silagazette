a894c84d89e312cb9d64519487680551b2464ab5
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2023-12-31 20:49:26 +0800

    feat: add basic sharing of post

--- src/components/blog/PostHeader.tsx
+++ src/components/blog/PostHeader.tsx
@@ -2,9 +2,13 @@ import TagsList from './tags/TagsList';
 import getFormattedDate from '../functions/getFormattedDate';
 import { getPostData } from '../functions/getPostsData';
 import LinkText from '../LinkText';
+import Button from '../Button';
+import { PRODUCTION_DEPLOY_URL } from '@/constants';
 
 const PostHeader = (props: { slug: string }) => {
     const post = getPostData(props.slug);
+    const post_url = `${PRODUCTION_DEPLOY_URL}/blog/${props.slug}/`;
+
     return (
         <>
             <h1 className='my-8 font-serif text-4xl font-bold tracking-tight'>
@@ -23,6 +27,25 @@ const PostHeader = (props: { slug: string }) => {
             </div>
 
             <TagsList tags={post.data.tags} icon />
+
+            <div className='flex items-center gap-2 pt-4 text-slate-500'>
+                <div className='text-sm'>Share this article</div>
+                <i className='bx bx-share-alt '></i>
+                <Button
+                    href={`https://www.facebook.com/sharer/sharer.php?u=${post_url}`}
+                    target='_blank'
+                    className='inline'
+                >
+                    <i className='bx bxl-facebook '></i>
+                </Button>
+                <Button
+                    href={`https://www.linkedin.com/sharing/share-offsite/?url=${post_url}`}
+                    target='_blank'
+                    className='inline'
+                >
+                    <i className='bx bxl-linkedin'></i>
+                </Button>
+            </div>
         </>
     );
 };
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 // Limit to the number of posts shown per page
 export const POSTS_PER_PAGE = 5;
+export const PRODUCTION_DEPLOY_URL =
+    'https://singapore-international-law-gazette.vercel.app';
