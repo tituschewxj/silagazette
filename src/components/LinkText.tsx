1507b9ea938ad3d320681a1449ac73c19862a9bb
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2023-12-31 21:21:34 +0800

    refactor: LinkText

--- src/components/LinkText.tsx
+++ src/components/LinkText.tsx
@@ -1,18 +1,14 @@
 import Link from 'next/link';
 
 const LinkText = (props: { link?: string; text: string }) => {
-    return (
-        <>
-            {props.link ? (
-                <Link href={props.link} className='text-sm text-slate-500'>
-                    <div className='absolute z-20 inline underline transition duration-200 hover:text-red-700'>
-                        {props.text}
-                    </div>
-                </Link>
-            ) : (
-                <>{props.text}</>
-            )}
-        </>
+    return props.link ? (
+        <Link href={props.link}>
+            <div className='absolute z-20 inline text-slate-500 underline transition duration-200 hover:text-red-700'>
+                {props.text}
+            </div>
+        </Link>
+    ) : (
+        <>{props.text}</>
     );
 };
 export default LinkText;
