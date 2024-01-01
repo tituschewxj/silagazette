e238793c09b2aee2173889f6167a3bd45046318c
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2024-01-01 13:57:26 +0800

    feat: add small sidebar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,12 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
+import Sidebar from './Sidebar';
 
 const Header = () => {
+    const [state, setState] = useState({ isSidebarActive: false });
+
     return (
         <header className='bg-gradient-to-r from-rose-800 via-red-800 to-red-900 p-4 text-white shadow-md'>
             <div className='container mx-auto flex items-center justify-between'>
@@ -12,7 +18,7 @@ const Header = () => {
                 </Link>
 
                 {/* <!-- Navigation --> */}
-                <nav className='flex items-center space-x-4'>
+                <nav className='hidden items-center space-x-4 sm:flex'>
                     <Link href='/blog' className='hover:text-gray-300'>
                         Blog
                     </Link>
@@ -24,9 +30,29 @@ const Header = () => {
                     </Link>
                     {/* TODO: Search feature */}
                     {/* <i className='bx bx-search text-xl'></i> */}
-
-                    {/* TODO: Sidebar */}
                 </nav>
+                <div className='relative sm:hidden'>
+                    {state.isSidebarActive ? (
+                        <i
+                            className='bx bx-x text-2xl'
+                            onClick={() =>
+                                setState({
+                                    isSidebarActive: false,
+                                })
+                            }
+                        ></i>
+                    ) : (
+                        <i
+                            className='bx bx-menu text-2xl'
+                            onClick={() =>
+                                setState({
+                                    isSidebarActive: true,
+                                })
+                            }
+                        ></i>
+                    )}
+                    {state.isSidebarActive && <Sidebar></Sidebar>}
+                </div>
             </div>
         </header>
     );
---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+const Sidebar = () => {
+    return (
+        <nav className='shadow:xl absolute right-0 z-30 flex flex-col gap-8 rounded-xl  bg-red-900 p-10 sm:hidden'>
+            <Link href='/' className='hover:text-gray-300'>
+                Home
+            </Link>
+            <Link href='/blog' className='hover:text-gray-300'>
+                Blog
+            </Link>
+            <Link href='/about' className='hover:text-gray-300'>
+                About
+            </Link>
+            <Link href='/contact' className='hover:text-gray-300'>
+                Contact
+            </Link>
+        </nav>
+    );
+};
+
+export default Sidebar;
