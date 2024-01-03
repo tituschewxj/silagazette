3199496905cd28ec38e580cbc9a0337541709a45
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2024-01-03 15:35:57 +0800

    feat: onClick function in button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,18 @@
 import Link from 'next/link';
 
 const Button = (props: {
-    href: string;
+    href?: string;
     target?: string;
+    onClick?: () => void;
     children?: React.ReactNode;
     className?: string;
 }) => {
     return (
         <div className={props.className}>
             <Link
-                href={props.href}
-                target={props.target}
+                href={props.href ? props.href : ''}
+                target={props.href ? props.target : undefined}
+                onClick={props.onClick && props.onClick}
                 className='rounded-full bg-white px-4 py-2 font-semibold text-red-700  shadow-md transition duration-200 hover:bg-gray-100 hover:text-red-600'
             >
                 {props.children}
