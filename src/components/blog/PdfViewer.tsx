27138d7c79b7e6c0c0e06d66bf58b75d5aa5566c
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2024-01-03 16:07:17 +0800

    feat: add loading icon to PdfViewer

--- src/components/blog/PdfViewer.tsx
+++ src/components/blog/PdfViewer.tsx
@@ -3,9 +3,10 @@
 import { PostData } from '@/types/PostMetadata';
 import Button from '../Button';
 import { useState } from 'react';
+import Loading from '../Loading';
 
 const PdfViewer = (props: { post: PostData }) => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [state, setState] = useState({ isOpen: false, hasLoaded: false });
 
     return (
         <>
@@ -14,14 +15,9 @@ const PdfViewer = (props: { post: PostData }) => {
                 <Button
                     className='py-5 text-sm'
                     onClick={() => {
-                        console.log(window.matchMedia('(max-width: 640px)'));
-                        if (window.matchMedia('(max-width: 640px)').matches) {
-                            // open in new window
-                            console.log('new window');
-                            window.open(props.post.data.pdf_link);
-                        } else {
-                            setIsOpen(true);
-                        }
+                        window.matchMedia('(max-width: 640px)').matches
+                            ? window.open(props.post.data.pdf_link) // open in new window
+                            : setState({ ...state, isOpen: true });
                     }}
                     // href={post.data.pdf_link}
                     target='_blank'
@@ -29,19 +25,35 @@ const PdfViewer = (props: { post: PostData }) => {
                     View PDF version of this article
                 </Button>
             )}
-            {/* TODO: add loading indicator */}
-            <div hidden={!isOpen}>
+
+            <div hidden={!state.isOpen}>
                 <div
-                    onClick={() => {
-                        setIsOpen(false);
-                    }}
+                    onClick={() => setState({ ...state, isOpen: false })}
                     className='fixed inset-x-0 inset-y-0 z-10 hidden items-center rounded-md backdrop-blur-sm sm:flex'
                 >
-                    <iframe
-                        src={props.post.data.pdf_link}
-                        className='z-10 mx-auto aspect-auto h-[85%] w-full max-w-3xl rounded-2xl border-white bg-white p-2 shadow-2xl'
-                        loading='lazy'
-                    ></iframe>
+                    <div className='relative z-10 mx-auto aspect-auto h-[85%] w-full max-w-3xl rounded-2xl border-white bg-slate-50 p-2 align-middle shadow-2xl'>
+                        <Button
+                            className='absolute right-[-1.5em] top-[-0.5em] z-20'
+                            onClick={() =>
+                                setState({ ...state, isOpen: false })
+                            }
+                        >
+                            <i className='bx bx-x text-xl'></i>
+                        </Button>
+                        {!state.hasLoaded && (
+                            <div className='mx-auto flex h-full items-center justify-center'>
+                                <Loading></Loading>
+                            </div>
+                        )}
+                        <iframe
+                            src={props.post.data.pdf_link}
+                            className='h-full w-full rounded-xl'
+                            loading='lazy'
+                            onLoad={() =>
+                                setState({ ...state, hasLoaded: true })
+                            }
+                        ></iframe>
+                    </div>
                 </div>
             </div>
         </>
