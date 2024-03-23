09e3fac08fa823be5f063d20f6bee7c56aab384f
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2024-03-23 18:31:00 +0800

    feat: update about page with Person type

--- src/components/PeopleRoles.tsx
+++ src/components/PeopleRoles.tsx
@@ -1,14 +1,42 @@
-const PeopleRoles = (props: { names: string[]; position: string }) => {
+import { Person } from '@/types/Person';
+import LinkText from './LinkText';
+
+/**
+ * Renders the a cell like output of the position in bold, followed by the names of the people with that position.
+ *
+ * @param props Contains the position, and people names.
+ */
+const PeopleRoles = (props: {
+    position: string;
+    people: string[] | Person[];
+}) => {
     return (
         <div className='text-center'>
             <div className='font-semibold text-slate-800'>{props.position}</div>
-            {props.names.map((name) => {
-                return (
-                    <div id={name} className='text-slate-600'>
-                        {name}
-                    </div>
-                );
-            })}
+            {props.people &&
+                props.people.map((person, idx) => {
+                    if (typeof person == 'string') {
+                        return (
+                            <div
+                                key={idx.toString()}
+                                className='text-slate-600'
+                            >
+                                {person}
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div key={idx.toString()}>
+                                <LinkText
+                                    text={person.name}
+                                    link={person.profile_link}
+                                    new_tab
+                                    className='text-slate-600'
+                                ></LinkText>
+                            </div>
+                        );
+                    }
+                })}
         </div>
     );
 };
